d18b5fb84c3356b59906088ffa7adad1e0a792a9
raketo <tobias.rakette@gmx.de>
2015-09-11 08:03:18 +0200

    Wiederherstellung der Servicedateien

---
+++ Hello.WCF-Asp.Net/Hello.WCF/IMessengerService.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using Hello.WCF.Dataobjects;
+
+namespace Hello.WCF
+{
+    // HINWEIS: Mit dem Befehl "Umbenennen" im Menü "Umgestalten" können Sie den Schnittstellennamen "IMessengerService" sowohl im Code als auch in der Konfigurationsdatei ändern.
+    [ServiceContract(Namespace="")]
+    public interface IMessengerService
+    {
+        #region CreateMethods
+        [OperationContract]
+        [WebInvoke(
+             Method = "Post",
+             UriTemplate = "CreateUser",
+             BodyStyle = WebMessageBodyStyle.WrappedRequest,
+             ResponseFormat = WebMessageFormat.Json,
+             RequestFormat = WebMessageFormat.Json)]
+        void CreateUser(User user);
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "Post",
+            UriTemplate = "CreateMessage",
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json)]
+        void CreateMessage(Message message);
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "Post",
+            UriTemplate = "CreateMessage",
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json)]
+        void CreateRelationship(Relationship relationship);
+        #endregion
+
+        #region ReadMethods
+        [OperationContract]
+        [WebGet(
+            UriTemplate = "GetRelationship",
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json)]
+        IList<User> GetRelationship(Guid userId);
+
+        [OperationContract]
+        [WebGet(
+            UriTemplate = "GetMessages",
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json)]
+        IList<Message> GetMessages(Guid userId);
+
+        //[OperationContract]
+        //[WebGet(
+        //    UriTemplate = "GetAllUsers",
+        //    BodyStyle = WebMessageBodyStyle.WrappedRequest,
+        //    ResponseFormat = WebMessageFormat.Json,
+        //    RequestFormat = WebMessageFormat.Json)]
+        //IList<User> GetAllUsers();
+
+        [OperationContract]
+        [WebGet(
+            UriTemplate = "GetUserByAccountName",
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json)]
+        User GetUserByAccountName(string AccountName);
+        #endregion
+
+        #region UpdateMethods
+        [OperationContract]
+        [WebInvoke(
+           Method = "Put",
+           UriTemplate = "UpdateUser",
+           BodyStyle = WebMessageBodyStyle.WrappedRequest,
+           ResponseFormat = WebMessageFormat.Json,
+           RequestFormat = WebMessageFormat.Json)]
+        User UpdateUser(User user);
+        #endregion
+
+        #region DeleteMethods
+        [OperationContract]
+        [WebInvoke(
+           Method = "Delete",
+           UriTemplate = "DeleteUser",
+           BodyStyle = WebMessageBodyStyle.WrappedRequest,
+           ResponseFormat = WebMessageFormat.Json,
+           RequestFormat = WebMessageFormat.Json)]
+        void DeleteUser(User user);
+
+        [OperationContract]
+        [WebInvoke(
+           Method = "Delete",
+           UriTemplate = "DeleteRelationship",
+           BodyStyle = WebMessageBodyStyle.WrappedRequest,
+           ResponseFormat = WebMessageFormat.Json,
+           RequestFormat = WebMessageFormat.Json)]
+        void DeleteRelationship(Relationship relationship);
+        #endregion
+    }
+}
---
+++ Hello.WCF-Asp.Net/Hello.WCF/MessengerService.svc.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Hello.WCF.Dataobjects;
+using Hello.WCF.BuisnessLogic;
+
+namespace Hello.WCF
+{
+    // HINWEIS: Wählen Sie zum Starten des WCF-Testclients zum Testen dieses Diensts MessengerService.svc oder MessengerService.svc.cs im Projektmappen-Explorer aus, und starten Sie das Debuggen.
+    public class MessengerService : IMessengerService
+    {
+        #region CreateMethods
+        public void CreateMessage(Message message)
+        {
+            MessageManager.CreateMessage(message);
+        }
+
+        public void CreateRelationship(Relationship relationship)
+        {
+            RelationshipManager.CreateRelationship(relationship);
+        }
+
+        public void CreateUser(User user)
+        {
+            UserManager.CreateUser(user);
+        }
+        #endregion
+
+        #region ReadMethods
+        //public IList<User> GetAllUsers()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public IList<User> GetRelationship(Guid userId)
+        {
+            List<Relationship> relationshipList = RelationshipManager.GetRelationships(userId);
+            List<User> friends = new List<User>();
+            foreach (Relationship friendId in relationshipList)
+            {
+                User friend = UserManager.GetUserByUserId(friendId.FriendsId);
+                friends.Add(friend);
+            }
+
+            return friends;
+        }
+
+        public IList<Message> GetMessages(Guid userId)
+        {
+            List<Message> messageList = MessageManager.ReadMessage();
+            foreach (Message msg in messageList)
+            {
+                MessageManager.MessageReaded(msg.Id);
+            }
+            return messageList;
+        }
+
+        public User GetUserByAccountName(string accountName)
+        {
+           User user = UserManager.GetUserByAccountName(accountName);
+           return user;
+        }
+        #endregion
+
+        #region UpdateMethod
+        public User UpdateUser(User user)
+        {
+            User useredit = UserManager.UpdateUser(user);
+            return useredit;
+        }
+        #endregion
+
+        #region DeleteMethods
+        public void DeleteRelationship(Relationship relationship)
+        {
+            RelationshipManager.DeleteRelationship(relationship);
+        }
+
+        public void DeleteUser(User user)
+        {
+            UserManager.DeleteUser(user);
+        }
+        #endregion
+    }
+}
